54dc1dd38ed10a421d2d112aa72500aaf8a641b2
Luke Rasmussen <luke.rasmussen@northwestern.edu>
2015-02-02 07:02:13 -0600

    Finished code refactoring for value set/term dialog

--- app/scripts/controllers/valueSetsTermsDialog.js
+++ app/scripts/controllers/valueSetsTermsDialog.js
@@ -1,5 +1,3 @@
-/* globals _ */
-
 'use strict';
 
 /**
@@ -10,38 +8,7 @@
  * Controller of the sopheAuthorApp
  */
 angular.module('sopheAuthorApp')
-.controller('ValueSetsTermsDialogController', ['$scope', '$http', '$modalInstance', 'ValueSetService', 'CodeSystemService', function ($scope, $http, $modalInstance, ValueSetService, CodeSystemService) {
-  $scope.termSearch = {term: '', isSearching: false, results: []};
-  $scope.valueSetSearch = {term: '', isSearching: false, results: []};
-  $scope.selectedTerms = [];
-  $scope.selectedValueSets = [];
-
-  $scope.$watch('valueSetSearch.term', function() {
-    ValueSetService.searchHelper($scope.valueSetSearch);
-  });
-  $scope.$watch('termSearch.term', function() {
-    CodeSystemService.searchHelper($scope.termSearch);
-  });
-
-  // Used for multi-selection mode
-  $scope.addToTermList = function(term) {
-    if (_.where($scope.selectedTerms, {id: term.id}).length === 0) {
-      $scope.selectedTerms.push(angular.copy(term));
-    }
-  };
-
-  $scope.removeFromTermList = function(term) {
-    $scope.selectedTerms = _.filter($scope.selectedTerms, function(item) {
-      return item.id !== term.id;
-    });
-  };
-
-  // Used for single-selection mode
-  $scope.setSelectedValueSet = function(valueSet) {
-    $scope.selectedValueSets[0] = valueSet;
-  };
-
-  // Used for modal dialog mode
+.controller('ValueSetsTermsDialogController', ['$scope', '$http', '$modalInstance', function ($scope, $http, $modalInstance) {
   $scope.ok = function () {
     $modalInstance.close({valueSets: $scope.selectedValueSets, terms: $scope.selectedTerms});
   };
