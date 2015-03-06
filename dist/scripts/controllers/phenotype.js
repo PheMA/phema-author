4c58a3f3c7c1150e27ef03f14b02a1d1e23fb408
Luke Rasmussen <luke.rasmussen@gmail.com>
2015-03-06 08:23:18 -0600

    Bug from restructuring - buttons not working

--- dist/scripts/controllers/phenotype.js
+++ dist/scripts/controllers/phenotype.js
@@ -19,23 +19,6 @@ angular.module('sopheAuthorApp')
     $scope.treeOptions = {
       dirSelectable: false
     };
-    $scope.buttons = [
-      {text: 'New', iconClass:'fa fa-plus', event: $scope.new, disabled: false, tooltip: 'Create a new phenotype'},
-      {text: 'Open', iconClass:'fa fa-folder-open', event: $scope.load, disabled: false, tooltip: 'Open and edit one of your existing phenotypes'},
-      {spacer: true},
-      {text: 'Save', iconClass:'fa fa-save', event: $scope.save, disabled: false, tooltip: 'Save changes to your phenotype'},
-      {text: 'Export', iconClass:'fa fa-arrow-circle-down', event: $scope.export, disabled: false, tooltip: 'Export the phenotype for use in an external application'},
-      {spacer: true},
-      {text: 'Copy', iconClass:'fa fa-copy', event: $scope.copy, disabled: true},
-      {text: 'Paste', iconClass:'fa fa-paste', event: $scope.paste, disabled: true},
-      {text: 'Undo', iconClass:'fa fa-undo', disabled: true},
-      {text: 'Redo', iconClass:'fa fa-repeat', disabled: true},
-      {spacer: true},
-      {text: 'Delete', iconClass:'fa fa-remove', event: $scope.delete, disabled: true, tooltip: 'Delete the highlighted element(s) in the canvas'},
-      {spacer: true},
-      {text: 'Feedback', iconClass:'fa fa-comment', event: $scope.delete, disabled: true, tooltip: 'Suggestions or comments'},
-    ];
-
 
     LibraryService.load()
       .then(LibraryService.processValues)
@@ -466,6 +449,25 @@ angular.module('sopheAuthorApp')
         });
       }
     };
+
+    // Keep this after the other scope function definitions - it needs to have the appropriate definitions loaded
+    // to associate with the buttons.
+    $scope.buttons = [
+      {text: 'New', iconClass:'fa fa-plus', event: $scope.new, disabled: false, tooltip: 'Create a new phenotype'},
+      {text: 'Open', iconClass:'fa fa-folder-open', event: $scope.load, disabled: false, tooltip: 'Open and edit one of your existing phenotypes'},
+      {spacer: true},
+      {text: 'Save', iconClass:'fa fa-save', event: $scope.save, disabled: false, tooltip: 'Save changes to your phenotype'},
+      {text: 'Export', iconClass:'fa fa-arrow-circle-down', event: $scope.export, disabled: false, tooltip: 'Export the phenotype for use in an external application'},
+      {spacer: true},
+      {text: 'Copy', iconClass:'fa fa-copy', event: $scope.copy, disabled: true},
+      {text: 'Paste', iconClass:'fa fa-paste', event: $scope.paste, disabled: true},
+      {text: 'Undo', iconClass:'fa fa-undo', disabled: true},
+      {text: 'Redo', iconClass:'fa fa-repeat', disabled: true},
+      {spacer: true},
+      {text: 'Delete', iconClass:'fa fa-remove', event: $scope.delete, disabled: true, tooltip: 'Delete the highlighted element(s) in the canvas'},
+      {spacer: true},
+      {text: 'Feedback', iconClass:'fa fa-comment', event: $scope.delete, disabled: true, tooltip: 'Suggestions or comments'},
+    ];
 
 
     $scope.$on('sophe-search-valuesets', function(evt, dataElement) {
