68ebb35cdb70c63fb48e0f48b1979f88aae298a0
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-02-11 10:15:15 -0600

    Updated dependencies

--- bower_components/angular-tree-control/angular-tree-control.js
+++ bower_components/angular-tree-control/angular-tree-control.js
@@ -242,8 +242,16 @@ if (typeof module !== "undefined" && typeof exports !== "undefined" && module.ex
                     };
 
                     //tree template
+                    $scope.isReverse = function() {
+                      return !($scope.reverseOrder === 'false' || $scope.reverseOrder === 'False' || $scope.reverseOrder === '' || $scope.reverseOrder === false);
+                    };
+
+                    $scope.orderByFunc = function() {
+                      return "'" + $scope.orderBy + "'";
+                    };
+
                     var templateOptions = {
-                        orderBy: $scope.orderBy ? ' | orderBy:orderBy:reverseOrder' : '',
+                        orderBy: $scope.orderBy ? " | orderBy:orderByFunc():isReverse()" : '',
                         ulClass: classIfDefined($scope.options.injectClasses.ul, true),
                         nodeChildren:  $scope.options.nodeChildren,
                         liClass: classIfDefined($scope.options.injectClasses.li, true),
