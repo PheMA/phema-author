4cec2449c90f441117be7dc6ee8e3e85ea30ed39
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-02-11 10:47:32 -0600

    Build latest version

--- dist/scripts/controllers/results.js
+++ dist/scripts/controllers/results.js
@@ -2,19 +2,11 @@
 
 /**
  * @ngdoc function
- * @name sopheAuthorApp.controller:PromptController
+ * @name sopheAuthorApp.controller:ResultsController
  * @description
- * # PromptController
+ * # ResultsController
  * Controller of the sopheAuthorApp
  */
 angular.module('sopheAuthorApp')
-  .controller('ResultsController', ['$scope', 'UnitService', function ($scope, UnitService) {
-    $scope.units = [];
-
-    UnitService.load()
-      .then(UnitService.processValues)
-      .then(function(units) {
-        $scope.units = units;
-        //$scope.result.units = $scope.result.units;
-      });
+  .controller('ResultsController', [function () {
   }]);
